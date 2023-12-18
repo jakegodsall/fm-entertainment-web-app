d41443a7fa06caaaf17edd2277bef500d6a26daf
jakegodsall <jake.edward.godsall@gmail.com>
2023-12-18 15:03:42 +0000

    initial commit

---
+++ src/test/java/com/jakegodsall/fmentertainmentwebappbackend/FmEntertainmentWebAppBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jakegodsall.fmentertainmentwebappbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FmEntertainmentWebAppBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
